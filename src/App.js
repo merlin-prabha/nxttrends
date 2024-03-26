f3047be5bc85b932a6a5b7c0a9fbe11d3c6c4254
merlin_prabha <merlinprabha19@gmail.com>
2024-03-26 03:46:09 +0000

    enhance

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+  removeAllCartItems = () => {
+    this.setState({cartList: []})
+  }
+
+  incrementCartItemQuantity = id => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachItem => {
+        if (eachItem.id === id) {
+          const updatedQuantity = eachItem.quantity + 1
+          return {...eachItem, quantity: updatedQuantity}
+        }
+        return eachItem
+      }),
+    }))
+  }
+
+  decrementCartItemQuantity = id => {
+    const {cartList} = this.state
+    const cartItem = cartList.find(eachItem => eachItem.id === id)
+    if (cartItem.quantity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachItem => {
+          if (eachItem.id === id) {
+            const updatedQuantity = eachItem.quantity - 1
+            return {...eachItem, quantity: updatedQuantity}
+          }
+          return eachItem
+        }),
+      }))
+    } else {
+      this.removeCartItem(id)
+    }
+  }
+
+  removeCartItem = id => {
+    const {cartList} = this.state
+    const filteredList = cartList.filter(each => each.id !== id)
+    this.setState({cartList: filteredList})
+  }
+
+  addCartItem = product => {
+    // this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    //   TODO: Update the code here to implement addCartItem
+    const {cartList} = this.state
+    const productObject = cartList.find(each => each.id === product.id)
+
+    if (productObject !== undefined) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (productObject.id === eachCartItem.id) {
+            const updatedQuantity = eachCartItem.quantity + 1
+
+            return {...eachCartItem, quantity: updatedQuantity}
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      const updatedCartList = [...cartList, product]
+      this.setState({cartList: updatedCartList})
+    }
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+          removeAllCartItems: this.removeAllCartItems,
+          incrementCartItemQuantity: this.incrementCartItemQuantity,
+          decrementCartItemQuantity: this.decrementCartItemQuantity,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,29 @@
+// Write your code here
+import './index.css'
+import CartContext from '../../context/CartContext'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      let total = 0
+      cartList.forEach(eachCartItem => {
+        total += eachCartItem.price * eachCartItem.quantity
+      })
+
+      return (
+        <div className="cart-summary-container">
+          <h1>
+            <span>Order Total:</span> Rs {total}{' '}
+          </h1>
+          <p>{cartList.length} Items in cart</p>
+          <button type="button" className="checkout-button">
+            Checkout
+          </button>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartSummary
